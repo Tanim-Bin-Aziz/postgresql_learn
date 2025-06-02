7251a3655caa3c0a3fa888493edb9acdced99490
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-02 12:59:31 +0600

    alter syntax working perfect

--- alter.sql
+++ alter.sql
@@ -1,2 +1,8 @@
 -- Active: 1748842987362@@127.0.0.1@5432@ph
+--get all items from table
 SELECT * FROM person2;
+
+--using alter syntax to create exist table new column
+
+ALTER TABLE person2
+            ADD COLUMN blood_group VARCHAR(3) DEFAULT 'def' NOT NULL;
