f64d1ad651f52809543e81ed973ee135925fd4a2
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-02 21:11:33 +0600

    postgres learning basic done

--- alter.sql
+++ alter.sql
@@ -1,8 +1,40 @@
--- Active: 1748842987362@@127.0.0.1@5432@ph
---get all items from table
-SELECT * FROM person2;
+-- Active: 1699467625194@@127.0.0.1@5432@ph@public
 
---using alter syntax to create exist table new column
+
+SELECT * from person4;
 
 ALTER TABLE person2
-            ADD COLUMN blood_group VARCHAR(3) DEFAULT 'def' NOT NULL;
+    ADD COLUMN email VARCHAR(25)  DEFAULT 'default@mail.com' NOT NULL;
+
+ALTER TABLE person2
+    DROP COLUMN email;
+
+ALTER TABLE person2
+    RENAME COLUMN age to user_age;
+
+ALTER TABLE person2
+    ALTER COLUMN user_name TYPE VARCHAR(50);
+
+ALTER TABLE person2
+    ALTER COLUMN user_age set UNIQUE;
+
+ALTER TABLE person2
+    ALTER COLUMN user_age drop NOT null;
+
+
+ALTER TABLE person2
+    ADD constraint unique_person2_user_age UNIQUE(user_age);
+
+
+ALTER TABLE person2
+    DROP constraint unique_person2_user_age;
+
+
+TRUNCATE TABLE person2;
+DROP TABLE person2;
+
+
+-- INSERT into person4 VALUES(10,'mizan', 45);
+
+
+
